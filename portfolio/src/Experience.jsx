0c30479ed05a68a7ee4991636a22b98e3163005b
AR247365 <lonewolf22081998@gmail.com>
2024-04-05 22:35:53 +0530

    Final

--- portfolio/src/Experience.jsx
+++ portfolio/src/Experience.jsx
@@ -48,7 +48,7 @@ export default function Experience() {
             position={[0, 1.56, -1.4]}
             rotation-x={-0.256}
           >
-            <iframe src="https://2d-portfolio.netlify.app/" />
+            <iframe src="https://portfolio-2d.netlify.app/" />
           </Html>
         </primitive>
 
